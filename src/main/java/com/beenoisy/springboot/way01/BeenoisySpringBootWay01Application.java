56555abe890aecef7b05850fbd669ea50564e544
ziyi.wang <wangziyi@didichuxing.com>
2016-08-13 13:21:59 +0800

    Init project, set spring boot init project. see this page: http://www.jianshu.com/p/5a98c8007d6f

---
+++ src/main/java/com/beenoisy/springboot/way01/BeenoisySpringBootWay01Application.java
@@ -0,0 +1,12 @@
+package com.beenoisy.springboot.way01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeenoisySpringBootWay01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeenoisySpringBootWay01Application.class, args);
+	}
+}
