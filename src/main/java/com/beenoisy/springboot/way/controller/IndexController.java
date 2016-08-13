3132b12d5b1bb11ca7f10aca77488df474d9da59
ziyi.wang <wangziyi@didichuxing.com>
2016-08-13 19:12:22 +0800

    01 create first controller

--- src/main/java/com/beenoisy/springboot/way/controller/IndexController.java
+++ src/main/java/com/beenoisy/springboot/way/controller/IndexController.java
@@ -1,7 +1,15 @@
 package com.beenoisy.springboot.way.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by BeeNoisy on 16/8/13.
  */
+@RestController //1
 public class IndexController {
+    @RequestMapping(value = "/") // 2
+    public String index() {
+        return "Hello Spring boot"; // 3
+    }
 }
