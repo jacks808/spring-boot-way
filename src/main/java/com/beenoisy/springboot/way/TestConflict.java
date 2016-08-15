33cc3e30595bba82e9a2a3f22ab5722b48b3563a
ziyi.wang <wangziyi@didichuxing.com>
2016-08-15 18:28:42 +0800

    package a executable jar.

---
+++ src/main/java/com/beenoisy/springboot/way/TestConflict.java
@@ -0,0 +1,14 @@
+package com.beenoisy.springboot.way;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by BeeNoisy on 16/8/15.
+ */
+@SpringBootApplication
+public class TestConflict {
+    public static void main(String[] args) {
+        SpringApplication.run(TestConflict.class, args);
+    }
+}
